254a4c82ba2f33af580bfc15fa751757c3d8623f
= <carter.noh08@gmail.com>
2023-05-16 11:14:18 -0600

    Initial prototype done but untested

---
+++ libs/load_cell_reader/load_cell_packet.py
@@ -0,0 +1,90 @@
+# Copyright (c) farm-ng, inc.
+#
+# Licensed under the Amiga Development Kit License (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://github.com/farm-ng/amiga-dev-kit/blob/main/LICENSE
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import annotations
+
+import time
+from enum import IntEnum
+from struct import pack
+from struct import unpack
+from farm_ng.canbus import canbus_pb2
+from farm_ng.core.stamp import timestamp_from_monotonic
+from farm_ng.core.timestamp_pb2 import Timestamp
+
+# TODO: add some comments about the CAN bus protocol
+DASHBOARD_NODE_ID = 0xE
+PENDANT_NODE_ID = 0xF
+BRAIN_NODE_ID = 0x1F
+SDK_NODE_ID = 0x2A
+
+
+class Packet:
+    """Base class inherited by all CAN message data structures."""
+
+    @classmethod
+    def from_can_data(cls, data, stamp: float):
+        """Unpack CAN data directly into CAN message data structure."""
+        obj = cls()  # Does not call __init__
+        obj.decode(data)
+        obj.stamp_packet(stamp)
+        return obj
+
+    def stamp_packet(self, stamp: float):
+        """Time most recent message was received."""
+        self.stamp: Timestamp = timestamp_from_monotonic("canbus/packet", stamp)
+
+    def fresh(self, thresh_s: float = 0.5):
+        """Returns False if the most recent message is older than ``thresh_s`` in seconds."""
+        return self.age() < thresh_s
+
+    def age(self):
+        """Age of the most recent message."""
+        return time.monotonic() - self.stamp.stamp
+
+
+class LoadCellTx(Packet):
+    """Load Cell voltage and/or force?."""
+
+    cob_id = 0x180 #TODO change this??
+
+    def __init__(self, meas_force: float = 0.0):
+        self.format = "<d" 
+        self.meas_force = meas_force
+        self.stamp_packet(time.monotonic())
+
+    def encode(self):
+        """Returns the data contained by the class encoded as CAN message data."""
+        return pack(self.format, self.meas_force)
+
+    def decode(self, data):
+        """Decodes CAN message data and populates the values of the class."""
+        self.meas_force = unpack(self.format, data)
+
+    def __str__(self):
+        return "LOAD CELL TX Force {:0.3f} @ time {}".format(self.meas_force, self.stamp.stamp) 
+
+
+def parse_load_cell_tx_proto(message: canbus_pb2.RawCanbusMessage) -> LoadCellTx | None: #TODO Annotate with message type?
+    """Parses a canbus message from the Feather microcontroller.
+    IFF the message came from the microcontrollerand contains LoadCellTx structure, formatting, and cobid.
+
+    Args:
+        message: The raw canbus message to parse.
+    Returns:
+        The parsed LoadcellTx message, or None if the message is not a valid LoadcellTx message.
+    """
+    # TODO: add some checkers, or make python CHECK_API
+    # TODO: Need to add check for data type back in, right now it'll accept anything coming in ad might crash if the data type is wrong.
+    if message.id != LoadCellTx.cob_id + SDK_NODE_ID:
+        return None
+    return LoadCellTx.from_can_data(message.data, stamp=message.stamp)
--- src/main.py
+++ src/main.py
@@ -3,8 +3,17 @@ import argparse
 import asyncio
 import os
 from typing import List
+from typing import Optional
 
-from load_cell_reader import ops
+import grpc
+from farm_ng.canbus import canbus_pb2
+from farm_ng.canbus.canbus_client import CanbusClient
+from load_cell_reader.load_cell_packet import LoadCellTx
+from load_cell_reader.load_cell_packet import parse_load_cell_tx_proto
+
+#Not sure if I need these yet
+from farm_ng.service import service_pb2
+from farm_ng.service.service_client import ClientConfig
 
 # import internal libs
 
@@ -24,15 +33,27 @@ Config.set("kivy", "keyboard_mode", "systemanddock")
 # kivy imports
 from kivy.app import App  # noqa: E402
 from kivy.lang.builder import Builder  # noqa: E402
+from kivy.graphics.texture import Texture  # noqa: E402
+from kivy.properties import StringProperty  # noqa: E402
 
 
 class LoadCellApp(App):
     """Base class for the main Kivy app."""
 
-    def __init__(self) -> None:
-        super().__init__()
+    # For Kivy labels I guess? 
+    load_cell_force = StringProperty("???")
 
-        self.counter: int = 0
+    def __init__(self, address: str, canbus_port: int, stream_every_n: int) -> None:
+        super().__init__()
+        self.address: str = address
+        self.canbus_port: int = canbus_port
+        self.stream_every_n: int = stream_every_n
+
+        # Received values
+        self.load_cell_tx: LoadCellTx = LoadCellTx()
+
+        # Parameters
+        self.meas_force: float = 0.0
 
         self.async_tasks: List[asyncio.Task] = []
 
@@ -51,36 +72,100 @@ class LoadCellApp(App):
             for task in self.async_tasks:
                 task.cancel()
 
-        # Placeholder task
-        self.async_tasks.append(asyncio.ensure_future(self.template_function()))
+        # configure the canbus client
+        canbus_config: ClientConfig = ClientConfig(
+            address=self.address, port=self.canbus_port
+        )
+        canbus_client: CanbusClient = CanbusClient(canbus_config)
 
-        return await asyncio.gather(run_wrapper(), *self.async_tasks)
+        # Canbus task(s)
+        self.async_tasks.append(
+            asyncio.ensure_future(self.stream_canbus(canbus_client))
+        )
+        # self.async_tasks.append(
+        #     asyncio.ensure_future(self.send_can_msgs(canbus_client))
+        # )
 
-    async def template_function(self) -> None:
-        """Placeholder forever loop."""
+    async def stream_canbus(self, client: CanbusClient) -> None:
+        """This task:
+
+        - listens to the canbus client's stream
+        - filters for LoadCellTx messages
+        - extracts useful values from LoadCellTx messages
+        """
         while self.root is None:
             await asyncio.sleep(0.01)
 
-        while True:
-            await asyncio.sleep(1.0)
+        response_stream = None
 
-            # increment the counter using internal libs and update the gui
-            self.counter = ops.add(self.counter, 1)
-            self.root.ids.counter_label.text = (
-                f"{'Tic' if self.counter % 2 == 0 else 'Tac'}: {self.counter}"
-            )
+        while True:
+            # check the state of the service
+            state = await client.get_state()
+
+            if state.value not in [
+                service_pb2.ServiceState.IDLE,
+                service_pb2.ServiceState.RUNNING,
+            ]:
+                if response_stream is not None:
+                    response_stream.cancel()
+                    response_stream = None
+
+                print("Canbus service is not streaming or ready to stream")
+                await asyncio.sleep(0.1)
+                continue
+
+            if (
+                response_stream is None
+                and state.value != service_pb2.ServiceState.UNAVAILABLE
+            ):
+                # get the streaming object
+                response_stream = client.stream()
+
+            try:
+                # try/except so app doesn't crash on killed service
+                response: canbus_pb2.StreamCanbusReply = await response_stream.read()
+                assert response and response != grpc.aio.EOF, "End of stream"
+            except Exception as e:
+                print(e)
+                response_stream.cancel()
+                response_stream = None
+                continue
+
+            for proto in response.messages.messages:
+                load_cell_tx: Optional[LoadCellTx] = parse_load_cell_tx_proto(proto)
+                if load_cell_tx:
+                    # Store the value for possible other uses
+                    self.load_cell_tx = load_cell_tx
+
+                    # Update the Label values as they are received
+                    self.load_cell_force = str(load_cell_tx.meas_force)
+                    self.root.ids.force_label.text = (
+                        f"{'Force from Load Cell'}: {self.load_cell_force}"
+                    )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="load_cell_reader_app")
-
-    # Add additional command line arguments here
-
+    parser.add_argument(
+        "--address", type=str, default="localhost", help="The server address"
+    )
+    parser.add_argument(
+        "--canbus-port",
+        type=int,
+        required=True,
+        help="The grpc port where the canbus service is running.",
+    )
+    parser.add_argument(
+        "--stream-every-n",
+        type=int,
+        default=1,
+        help="Streaming frequency (used to skip frames)",
+    )
     args = parser.parse_args()
 
     loop = asyncio.get_event_loop()
     try:
-        loop.run_until_complete(LoadCellApp().app_func())
+        loop.run_until_complete(LoadCellApp(args.address, args.canbus_port, args.stream_every_n).app_func())
     except asyncio.CancelledError:
         pass
     loop.close()
