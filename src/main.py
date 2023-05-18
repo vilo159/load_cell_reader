470c075c3a2dfb79ba4b28977510bf95d84553a5
= <carter.noh08@gmail.com>
2023-05-17 20:25:44 -0600

    start time fixed, working on resolution issue now

--- src/main.py
+++ src/main.py
@@ -45,7 +45,7 @@ from res.elements import *
 
 #--------------------------------- Load Cell Class & Execution ---------------------------------
 
-INTERVAL = .003
+INTERVAL = .001
 SECOND_CAP = 1/INTERVAL
 
 class LoadCellApp(App, BaseScreen):
